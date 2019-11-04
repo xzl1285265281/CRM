991c2250f29599ff7fc9efc83a60b7cdde43ca9f
xzl <18256025235@163.com>
2019-11-04 22:26:28 +0800

    Add Four java File

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class hello{
+	public static void main(String[] args) {
+		System.out.println("hello git!!!");
+		System.out.println("25252558");
+		System.out.println("1111");
+		System.out.println("22222");
+	}
+}
---
+++ hello.java
@@ -0,0 +1,10 @@
+public class hello{
+	public static void main(String[] args) {
+		System.out.println("hello git!");
+		System.out.println("给体贴!");
+		System.out.println("给他!");
+		System.out.println("给同1234!");
+		System.out.println("125252!");
+		System.out.println("128");
+	}		
+}
